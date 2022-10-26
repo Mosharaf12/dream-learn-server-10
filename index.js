f34f73c70b7cf240419d26e4f05d20d4e5abd369
unknown <mosharafctg111@gmail.com>
2022-10-26 11:13:26 +0600

    added the total api complete

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const port =process.env.PORT || 5000
 
 app.use(cors());
 
+const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/categories',(req, res)=>{
+    res.send(categories);
+})
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+})
+app.get('/category/:id',(req,res)=>{
+    const id= req.params.id;
+    const newCategory = courses.find(c=> c._id==id);
+    res.send(newCategory);
+
+
 })
 
 app.listen(port, () => {
